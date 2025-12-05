c4f24f23cccb6506936433af6a0acf048c981c22
Carlos Fuentes <me@metcoder.dev>
2025-12-05 12:57:00 +0100

    test: initial

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,58 @@
 'use strict'
+import { test, describe } from 'node:test'
+import assert from 'node:assert/strict'
+
+describe('FuseBox API', () => {
+  test('should import FuseBox and Workflows', async t => {
+    const {
+      default: FuseBox,
+      FuseBox: NamedFuseBox,
+      Workflows
+    } = await import('../index.js')
+
+    assert.strictEqual(
+      FuseBox,
+      NamedFuseBox,
+      'Default and named FuseBox should be the same'
+    )
+    assert.ok(Workflows, 'Workflows should be defined')
+    assert.ok(Workflows.Retry, 'Retry workflow should be defined')
+    assert.ok(Workflows.Fallback, 'Fallback workflow should be defined')
+    assert.ok(
+      Workflows.CircuitBreaker,
+      'CircuitBreaker workflow should be defined'
+    )
+  })
+
+  test('should create an instance of FuseBox (default)', async () => {
+    const { default: FuseBox, Workflows } = await import('../index.js')
+    const fusebox = new FuseBox()
+    assert.ok(
+      fusebox instanceof FuseBox,
+      'fusebox should be an instance of FuseBox'
+    )
+    assert.ok(
+      fusebox.addWorkflows(Workflows.CircuitBreaker(), Workflows.Retry())
+    )
+    assert.ok(
+      fusebox.addWorkflows([Workflows.CircuitBreaker(), Workflows.Retry()])
+    )
+    assert.ok(fusebox.protect(() => {}))
+  })
+
+  test('should create an instance of FuseBox (named)', async () => {
+    const { FuseBox, Workflows } = await import('../index.js')
+    const fusebox = new FuseBox()
+    assert.ok(
+      fusebox instanceof FuseBox,
+      'fusebox should be an instance of FuseBox'
+    )
+    assert.ok(
+      fusebox.addWorkflows(Workflows.CircuitBreaker(), Workflows.Retry())
+    )
+    assert.ok(
+      fusebox.addWorkflows([Workflows.CircuitBreaker(), Workflows.Retry()])
+    )
+    assert.ok(fusebox.protect(() => {}))
+  })
+})
